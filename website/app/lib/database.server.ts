a345719b04352d5eb0f5c86cf2d7e10d31c64d4b
Sam Salfi <samsalfi@yahoo.ca>
2024-03-22 00:10:52 -0300

    Added ssl to Kysely

--- website/app/lib/database.server.ts
+++ website/app/lib/database.server.ts
@@ -8,6 +8,7 @@ export const pool = new pg.Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   port: parseInt(process.env.POSTGRES_PORT ?? "5432"),
+  ssl: process.env.NODE_ENV === "production",
   max: 10,
 });
 

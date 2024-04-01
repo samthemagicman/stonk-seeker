1235f551aa163835777028addc5eda3d621ba366
Sam Salfi <samsalfi@yahoo.ca>
2024-04-01 13:53:50 -0300

    Updated dependency installer

--- worker/v1/install_dependencies.sh
+++ worker/v1/install_dependencies.sh
@@ -4,4 +4,8 @@ if [ ! -d "venv" ] && [ ! -d "venv/bin" ]; then
     python3 -m venv venv
 fi
 source "./venv/bin/activate"
-pip install -r requirements.txt
+
+pip install pika psycopg psycopg-binary praw cachier spacy
+python -m spacy download en_core_web_sm
+
+# pip install -r requirements.txt

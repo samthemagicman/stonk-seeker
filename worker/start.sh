33b5900c8566eae6b3cc46f9531fd1fef83c65c7
Sam Salfi <samsalfi@yahoo.ca>
2024-04-01 00:56:20 -0300

    Update start.sh

--- worker/start.sh
+++ worker/start.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
-# Check if virtual environment exists
 if [ ! -d "venv" ] && [ ! -d "venv/bin" ]; then
     # If not, create a virtual environment
     python3 -m venv venv
 fi
-# Activate the virtual environment
 source "./venv/bin/activate"
-# Install Python requirements
 pip install -r requirements.txt
-# python -m spacy download en_core_web_sm
-# Start Python script as a daemon
-
-python -u "v1/main copy.py" > output.log &
+until python -u "v1/main copy.py" > output.log; do
+    echo "The program crashed at `date +%H:%M:%S`. Restarting the script..."
+done

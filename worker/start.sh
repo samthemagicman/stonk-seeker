cc0883e626db9c31c6e5d3413b919f639319acf3
Sam Salfi <samsalfi@yahoo.ca>
2024-03-31 23:09:03 -0300

    Added workflow files

---
+++ worker/start.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+set -e
+
+# Check if virtual environment exists
+if [ ! -d "venv" ] && [ ! -d "venv/bin" ]; then
+    # If not, create a virtual environment
+    python3 -m venv venv
+fi
+# Activate the virtual environment
+source "./venv/bin/activate"
+# Install Python requirements
+pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+# Start Python script as a daemon
+python -u "v1/main copy.py" > output.log &

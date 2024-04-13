702ef3bd3138547891555bf9702230f39a3e1703
Sam Salfi <samsalfi@yahoo.ca>
2024-04-13 16:24:00 -0300

    Added logo

---
+++ website/src/app/_components/icons.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+export function StonkSeekerLogo(props: React.SVGAttributes<SVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 536.9499 221.4531"
+      height={100}
+      className="fill-black dark:fill-white"
+      preserveAspectRatio="none"
+      {...props}
+    >
+      <use href="/logo.svg#main"></use>
+    </svg>
+  );
+}
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,8 +1,11 @@
+import { StonkSeekerLogo } from "./_components/icons";
 import { Button } from "./_components/ui/button";
+import Image from "next/image";
 
 export default async function Home() {
   return (
     <main>
+      <StonkSeekerLogo />
       <h1 className="text-4xl font-bold">Welcome to tRPC</h1>
       <Button>Click me</Button>
     </main>
